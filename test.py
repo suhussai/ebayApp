edfa2740f1550a496aa122586dd2b51b223e73b6
suhussai <suhussai@ualberta.ca>
2015-12-22 20:22:35 -0700

    add example using getOrders

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 from ebaysdk.trading import Connection as Trading
+from bs4 import BeautifulSoup
 import json
 
 ids = ['','','','']
@@ -9,13 +10,29 @@ for fileName, index in [('appid',0), ('devid',1), ('certid',2), ('tokenid',3)]:
 
 try:    
     api = Trading(appid=ids[0], devid=ids[1], certid=ids[2], token=ids[3])
-    response = api.execute('GetMyeBaySelling', {'SoldList': {'DurationInDays' : '1'}})
-    #print(json.dumps(response.dict(), indent=2))
-    for transaction in response.dict()['SoldList']['OrderTransactionArray']['OrderTransaction']:
-        print(transaction['Transaction']['Item']['Title'])
+    # response = api.execute('GetMyeBaySelling', {'SoldList': {'DurationInDays' : '1'}})
+    # #print(json.dumps(response.dict(), indent=2))
+    # for transaction in response.dict()['SoldList']['OrderTransactionArray']['OrderTransaction']:
+    #     name = transaction['Transaction']['Item']['Title']
+    #     try:
+    #         item_price = transaction['Transaction']['TotalPrice']['value']
+    #     except:
+    #         item_price = "Not Found"
+
+    #     print(name + ", " + item_price)
+
+        
+    
+
     #response.dict()['SoldList']['OrderTransactionArray']['OrderTransaction'][0]['Transaction']['Item']['Title']
-    #print(response.dict())
-    #print(response.reply)
+
+
+    # response = api.execute('GetOrders',{'NumberOfDays':'1'})
+    # print(json.dumps(response.dict(), indent=2))
+    # print(response.dict())
+    # print(response.reply)
+
+
 except Exception as e:
     print(e)
 
