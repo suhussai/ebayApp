71a2846f3ca1d0590723da13d65540519ad9df24
suhussai <suhussai@ualberta.ca>
2015-12-23 20:09:40 -0700

    added some delay for visibility

--- getPage.sh
+++ getPage.sh
@@ -8,9 +8,9 @@ xdotool key "Return"
 sleep 2
 xdotool key ctrl+s
 WID=`xdotool search --title "Save As" | head -1`
-sleep 0.5
+sleep 1.5
 xdotool key "Return"
-sleep 0.5
+sleep 1.5
 xdotool key "Return"
 
 #http://xmodulo.com/simulate-key-press-mouse-movement-linux.html
