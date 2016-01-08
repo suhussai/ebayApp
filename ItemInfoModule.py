bc26fcfe2ed8ed28f4f75138c7cd1b7eb2eb3502
suhussai <suhussai@ualberta.ca>
2016-01-07 22:05:29 -0700

    fix api initialization

--- ItemInfoModule.py
+++ ItemInfoModule.py
@@ -9,9 +9,9 @@ class ItemInfoClass:
         self.days = days
         self.json_fileName = json_fileName
         self._ItemsSold = None
+        self.api = Trading(appid=ids[0], devid=ids[1], certid=ids[2], token=ids[3])
         try:
             fileHandler = open(self.json_fileName, 'r')
-            self.api = Trading(appid=ids[0], devid=ids[1], certid=ids[2], token=ids[3])
             self._ItemsSold = json.load(fileHandler)
             fileHandler.close()
         except:
