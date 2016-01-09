78da54a10c08324bf20b6479002a017246a8470d
suhussai <suhussai@ualberta.ca>
2016-01-09 16:02:17 -0700

    remove spaces and fix certain  lines

--- ItemsHeldModule.py
+++ ItemsHeldModule.py
@@ -1,7 +1,7 @@
 import json
 
 class ItemsHeldClass:
-    
+
     def __init__(self, json_fileName):
         self.json_fileName = json_fileName
         try:
@@ -16,12 +16,12 @@ class ItemsHeldClass:
             'long_name': long_name,
             'short_name': short_name,
             'cost_of_item': cost_of_item
-        }               
-        
-        
+        }
+
+
     def delete_entry(self, long_name):
         self.ItemsHeld.pop(long_name.replace(" ", ""), None)
-        
+
     def get_short_name(self, long_name):
         value = self.ItemsHeld.get(long_name, None)
         if value is not None:
@@ -35,7 +35,7 @@ class ItemsHeldClass:
             return value.get("cost_of_item", "")
         else:
             return ""
-            
+
     def update_json_file(self):
         fileHandler = open(self.json_fileName, 'w')
         json.dump(self.ItemsHeld, fileHandler, indent=2)
@@ -46,7 +46,7 @@ class ItemsHeldClass:
         # 4x Wheel Center Hub Caps FOR Tacoma and 4Runner (1996-2002) 15"/16" Rims, 6 lugs\t4 pcs tundra caps\t$10.00\n
         update_fileHandler = open(update_file, 'r')
         content = update_fileHandler.read()
-        
+
         for line in content.split('\n'):
             values = line.split('\t')
             if len(values) == 3:
@@ -56,13 +56,11 @@ class ItemsHeldClass:
                 self.ItemsHeld[long_name.replace(" ","")] = {'long_name': long_name,
                                                    'short_name': short_name,
                                              'cost_of_item': cost_of_item
-                                        }               
+                                        }
             else:
-                print("found problem line " + line)
-        s = 'TWO WHEEL CENTER HUB CAPS For 07-15 CADILLAC ESCALADE 22 " PLAIN  CREST  9596649'.replace(" ","")
-        print(self.ItemsHeld.get(s), "Nope")
+                print("Error with line: " + line)
         self.update_json_file()
-    
 
-        
-        
+
+
+
