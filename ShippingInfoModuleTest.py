4b126c21424df8d8a440b75021f71633d3b37d02
suhussai <suhussai@ualberta.ca>
2016-01-21 12:16:23 -0700

    skeleton test class for shipping info module

---
+++ ShippingInfoModuleTest.py
@@ -0,0 +1,18 @@
+import unittest, os
+from ShippingInfoModule import ShippingInfoClass
+
+json_fileName = "ShippingInfoClassTest.json"
+class ShippingInfoClassTest(unittest.TestCase):
+
+    def setUp(self):
+        # create object
+        pass
+
+    def tearDown(self):
+        # nullify object
+        # and delete record folder
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
