7d92d2d343ddf5ebe5f8506e3b30c954e124dcb3
suhussai <suhussai@ualberta.ca>
2016-01-21 00:13:28 -0700

    silliest/costliest mistake EVER!

--- pathFunction.py
+++ pathFunction.py
@@ -13,4 +13,4 @@ def resource_path(relative_path):
     except Exception:
         base_path = os.path.abspath(".")
 
-        return os.path.join(base_path, relative_path)
+    return os.path.join(base_path, relative_path)
