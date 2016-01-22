132cce252094ff9f157ed037c9110d9ebe41cac8
suhussai <suhussai@ualberta.ca>
2016-01-21 20:02:58 -0700

    add basic tests for Shipping info module

--- ShippingInfoModuleTest.py
+++ ShippingInfoModuleTest.py
@@ -6,12 +6,29 @@ class ShippingInfoClassTest(unittest.TestCase):
 
     def setUp(self):
         # create object
-        pass
+        self.sic = ShippingInfoClass(json_fileName)
 
     def tearDown(self):
         # nullify object
         # and delete record folder
-        pass
+        self.sic = None
+        os.remove(json_fileName)
+
+    def test_add_entry(self):
+        self.sic.add_entry("some keys", "shipping info")
+        self.assertTrue(
+            self.sic.get_shipping_info("some keys") == "shipping info"
+        )
+
+    def test_delete_entry(self):
+        self.sic.add_entry("some keys", "shipping info")
+        self.assertTrue(
+            self.sic.get_shipping_info("some keys") == "shipping info"
+        )
+        self.sic.delete_entry("some keys")
+        self.assertTrue(
+            self.sic.get_shipping_info("some keys") == ""
+        )
 
 
 if __name__ == '__main__':
